b120e5b96df5b9895b1525037726de383a9e78fa
LoveSy <shana@zju.edu.cn>
2025-01-24 01:21:49 +0800

    Add back targetSdk to manifest

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,4 +1,4 @@
-import com.android.build.api.dsl.ManagedVirtualDevice
+import com.android.build.api.artifact.SingleArtifact
 
 plugins {
     alias(libs.plugins.agp.lib)
@@ -54,6 +54,38 @@ dependencies {
     androidTestImplementation(libs.test.rules)
     androidTestCompileOnly(projects.stub)
 }
+
+@CacheableTask
+abstract class ManifestUpdater : DefaultTask() {
+    @get:InputFile
+    @get:PathSensitive(PathSensitivity.RELATIVE)
+    abstract val mergedManifest: RegularFileProperty
+
+    @get:OutputFile
+    abstract val outputManifest: RegularFileProperty
+
+    @TaskAction
+    fun taskAction() {
+        outputManifest.get().asFile.writeText(
+            mergedManifest.get().asFile.readText()
+                .replace("<uses-sdk ", "<uses-sdk android:targetSdkVersion=\"35\" ")
+        )
+    }
+}
+
+
+androidComponents.onVariants { variant ->
+    val variantName = variant.name
+    val manifestUpdater =
+        project.tasks.register("${variantName}ManifestUpdater", ManifestUpdater::class.java)
+    variant.artifacts.use(manifestUpdater)
+        .wiredWithFiles(
+            ManifestUpdater::mergedManifest,
+            ManifestUpdater::outputManifest
+        )
+        .toTransform(SingleArtifact.MERGED_MANIFEST)
+}
+
 
 val repo = jgit.repo(true)
 version = repo?.latestTag?.removePrefix("v") ?: "0.0"
